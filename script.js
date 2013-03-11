87e2defd90b23bde5a25129c947e1619a36736a3
Nathan McDonald <nmcdonald00@gmail.com>
2013-03-11 18:16:19 -0300

    made velocity slightly random when it collides with the paddle, to prevent the circumstance of not being able to hit a a brick due to a static angle

--- script.js
+++ script.js
@@ -17,15 +17,11 @@ window.cancelRequestAnimFrame = ( function() {
 		window.msCancelRequestAnimationFrame || 
 		clearTimeout 
 })();
-
-
 /*
-	TODO collision detection
-			make ball travel in the left or right direction when it hits the left and right edges of paddle
+	TODO
 		increase ball speed as play progress
-
-
 */
+var loopInterval;
 var BRICK_WIDTH = 150;
 var BRICK_HEIGHT = 40;
 
@@ -37,9 +33,6 @@ var ctx = canvas.getContext("2d");
 var W = window.innerWidth; // windows width
 var H = window.innerHeight; // windows height
 
-
-// var W = 1000; // windows width
-// var H = 600; // windows height
 
 //set the canvas to the window h w
 canvas.width = W;
@@ -57,13 +50,16 @@ var bricks = [];
 var brickColors = ["red","yellow","green","blue","purple"];
 var keyColors = [122,120,99,118,98];
 var mouse = {}; //mouse object
+var score;
 
-
+var isGameOver = false;
 
 //init paddle
 var paddle = new Paddle(brickColors[0]);
 //init ball
 var ball = new Ball(paddle.x + paddle.width/2,paddle.y-paddle.height-5,4,8,6,brickColors[0]);
+
+var score = new Score(W - 100, 20,"white");
 
 placeBricks();
 //-----------------------render loop
@@ -77,49 +73,51 @@ render();
 //-----------------------game loop
 function update(){
 	//move the ball
+		//console.log(ball.vx);
+		// ball.vx+=0.0001;
+		// ball.vy+=0.0001;
 		ball.x += ball.vx;
 		ball.y += ball.vy;
+
 
 		// If the ball hits the bottom, run gameOver()
 		if (ball.y-ball.r*2 + ball.r > H){
 			// ball.vy = -ball.vy;
 			// ball.y = H - ball.r; 
-			//gameOver();
+			gameOver();
 		//if the ball hits the top or the top/bottom of a brick reverse y direction	
-		}else if((ball.y < 0 + ball.r) || collisionYBricks()){
+		}else if((ball.y < 0 + ball.r) || collisionYBricks()){	
+
 			ball.vy = -ball.vy;
 		}
 		//if ball strikes the vertical walls or the left/right of brick, invert the x-velocity vectory of ball
-		if ((ball.x + ball.r > W)||collisionXBricks()) { 
+		if ((ball.x + ball.r > W) || collisionXBricks()) { 
 			ball.vx = -ball.vx;
-		}else if((ball.x - ball.r < 0)||collisionXBricks()){
+		}else if((ball.x - ball.r < 0) || collisionXBricks()){
 			ball.vx = -ball.vx;
 		}
 
 		
 		//paddle/brick collision detection
 		if(paddleCollides(ball,paddle)){
-
+			rndSpeedY();
 			//send ball in the direction of theleft and right edges of the paddle
 			pRightEdge = paddle.x+paddle.width-30;
 			pLeftEdge = paddle.x+30;
 			if((ball.x+ball.r > paddle.x)&&(ball.x-ball.r < pLeftEdge)){
+				rndSpeedX();
 				if(!(ball.vx < 0)){
 					ball.vx = -ball.vx;	
 				}
 			}
 			if((ball.x < paddle.x+paddle.width)&&(ball.x > pRightEdge)){
+				rndSpeedX();
 				if(!(ball.vx > 0)){
 					ball.vx = -ball.vx;	
 				}
 			}
 			ball.vy = -ball.vy;
-			
-
 		}
-		
-		
-		
 }
 loopInterval = setInterval("requestAnimFrame(render)", 1000 / targetFps);
 
@@ -130,7 +128,7 @@ document.addEventListener("keypress",changeColor, true);
 
 // --------------------------------------------------------functions/classes
 
-//------collision detection
+//------------------------------collision detection
 
 function collisionYBricks(){
 	for(var i =0; i < bricks.length; i++){
@@ -145,6 +143,7 @@ function collisionYBricks(){
             if (ball.x + ball.vx + ball.r >= bricks[i].x && 
                 ball.x + ball.vx - ball.r<= bricks[i].x + bricks[i].width){
             	bricks.splice(i,1);
+            	score.updateScore(10);
             	return true;
             }
     	}
@@ -166,8 +165,8 @@ function collisionXBricks(){
 		    if ((ball.y + ball.vy -ball.r<= bricks[i].y + bricks[i].height) &&
 		        (ball.y + ball.vy + ball.r >= bricks[i].y)){                                                   
 		    	bricks.splice(i,1);
+		    	score.updateScore(10);	
 		        return true;
-		    	
 		    }
 		}
 	}
@@ -186,7 +185,7 @@ function collisionXBricks(){
     	}
 	}
 }
-
+//------------------------------objects
 function Ball(myX,myY,myVx,myVy,mySize,myColor){
 	this.x = myX;
 	this.y = myY;
@@ -231,25 +230,23 @@ function Brick(posX,posY,myColor){
 	}
 }
 
-function changeColor(e){
-	console.log("HELLO");
-	if(e.keyCode == keyColors[0]){
-		paddle.c = brickColors[0];
-		ball.c = brickColors[0];
-	}else if(e.keyCode == keyColors[1]){
-		paddle.c = brickColors[1];
-		ball.c = brickColors[1];
-	}else if(e.keyCode == keyColors[2]){
-		paddle.c = brickColors[2];
-		ball.c = brickColors[2];
-	}else if(e.keyCode == keyColors[3]){
-		paddle.c = brickColors[3];
-		ball.c = brickColors[3];
-	}else if(e.keyCode == keyColors[4]){
-		paddle.c = brickColors[4];
-		ball.c = brickColors[4];
+function Score(posX,posY,myColor){
+	this.score = 0;
+	this.x = posX;
+	this.y = posY;
+	this.c = myColor;
+	this.draw = function(){
+		ctx.fillStyle = "white";
+		ctx.font = "20px Arial, sans-serif";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillText("Score: "+ this.score, this.x, this.y + 25);
+	}
+	this.updateScore = function(newScore){
+		this.score += newScore;
 	}
 }
+//------------------------------functions
 
 function calcNumOfCols(screenW){
 	var num = W;
@@ -275,7 +272,7 @@ function placeBricks(){
 	}
 
 	var leftPadding = (W - 1050)/ 2;
-	var row = 0;
+	var row = 80;
 	var column = leftPadding;
 	for (var i = 0; i < 56; i++) {
 		var bColor = Math.floor(Math.random()*5);
@@ -306,8 +303,22 @@ function draw(){
 		bricks[i].draw();
 	}
 	ball.draw();
+	score.draw();
+	if(isGameOver){
+		drawGameOver();
+	}
+	
 }
 
+function drawGameOver(){
+	ctx.fillStyle = "white";
+	ctx.font = "160px Arial, sans-serif";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillText("GAME OVER", W/2, H/2 + 25);
+}
+
+//------------------------------event handlers
 function trackPosition(e){
 	var distanceR;
 	var distanceL;
@@ -328,17 +339,45 @@ function trackPosition(e){
 }
 
 function gameOver(){
-	ctx.fillStyle = "white";
-	ctx.font = "20px Arial, sans-serif";
-	ctx.textAlign = "center";
-	ctx.textBaseline = "middle";
-	ctx.fillText("Game Over", W/2, H/2 + 25);
-	cancelRequestAnimFrame(init);
+	isGameOver = true;
+	clearInterval(loopInterval);
 }
 
+function changeColor(e){
+	if(e.keyCode == keyColors[0]){
+		paddle.c = brickColors[0];
+		ball.c = brickColors[0];
+	}else if(e.keyCode == keyColors[1]){
+		paddle.c = brickColors[1];
+		ball.c = brickColors[1];
+	}else if(e.keyCode == keyColors[2]){
+		paddle.c = brickColors[2];
+		ball.c = brickColors[2];
+	}else if(e.keyCode == keyColors[3]){
+		paddle.c = brickColors[3];
+		ball.c = brickColors[3];
+	}else if(e.keyCode == keyColors[4]){
+		paddle.c = brickColors[4];
+		ball.c = brickColors[4];
+	}
+}
 
 function changeWindowSize(e){
 	H = window.innerHeight;
 	W = window.innerWidth;
 }
 
+function rndSpeedX(){
+	if(ball.vx < 0){
+		ball.vx = -Math.random()*(4-3+1)+3;
+	}else{
+		ball.vx = Math.random()*(4-3+1)+3;
+	}
+}
+function rndSpeedY(){
+	if(ball.vy < 0){
+		ball.vy = -Math.random()*(9-7+1)+7;
+	}else{
+		ball.vy = Math.random()*(9-7+1)+7;
+	}
+}
